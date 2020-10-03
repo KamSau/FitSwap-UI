87d9ab7eead22f32675f82711754f4a31aa8f0ea
Esteban <esteban.salom@gmail.com>
2020-10-03 00:58:07 -0600

    commit con conexion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState("");
+  useEffect(() => {
+    if (state === "") {
+      axios.get(`http://localhost:8080/api/v1/test`).then((res) => {
+        setState(res.data);
+      });
+    }
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {state === "" ? "Waiting for connection" : "Connection established!"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
